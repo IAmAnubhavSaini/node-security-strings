3e43fc081721241fa6465b537dc6d08ed0731c55
Anubhav <iamanubhavsaini@gmail.com>
2021-01-22 04:21:50 +0530

    secstr command

---
+++ src/strings.js
@@ -0,0 +1,30 @@
+#!/usr/bin/env node
+
+const blue = require('@f0c1s/color-blue')
+const green = require('@f0c1s/color-green')
+const red = require('@f0c1s/color-red')
+const yellow = require('@f0c1s/color-yellow')
+const escapeHTML = require('@f0c1s/escape-html')
+const escapeJS = require('@f0c1s/escape-javascript')
+const {rot47, rotations} = require('@f0c1s/security-rot')
+const base64 = require('@m1yh3m/base.64.codec.node.lib')
+
+const input = process.argv[2]
+console.log(green(`for input: ${blue(input)}`))
+let names = ['rot47', 'base64.encode', 'base64.decode', 'escapeJS.escape', 'escapeHTML.escape']
+let fns = [rot47, base64.encode, base64.decode, escapeJS.escape, escapeHTML.escape]
+let out = fns.map(fn => {
+    let val = ''
+    try {
+        val = fn(input)
+    } catch (e) {
+        val = 'not applicable'
+    }
+    return val
+})
+out.forEach((v, i) => {
+    console.log(blue(input), green(names[i]), red(v))
+})
+
+console.log(`rot13 rotations on: ${blue(input)}`)
+rotations(input).forEach(i => console.log(i))
