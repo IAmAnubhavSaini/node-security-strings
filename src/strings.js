b731ff5386d4a534af42054e855aaf00d2cda4a5
Anubhav <iamanubhavsaini+github@gmail.com>
2021-05-24 06:59:25 +0530

    v2022 - bumped because I touched it a little

--- src/strings.js
+++ src/strings.js
@@ -1,30 +1,29 @@
 #!/usr/bin/env node
 
-const blue = require('@f0c1s/color-blue')
-const green = require('@f0c1s/color-green')
-const red = require('@f0c1s/color-red')
-const yellow = require('@f0c1s/color-yellow')
-const escapeHTML = require('@f0c1s/escape-html')
-const escapeJS = require('@f0c1s/escape-javascript')
-const {rot47, rotations} = require('@f0c1s/security-rot')
-const base64 = require('@m1yh3m/base.64.codec.node.lib')
+const blue = require('@f0c1s/color-blue');
+const green = require('@f0c1s/color-green');
+const red = require('@f0c1s/color-red');
+const {escape: escapeHTML} = require('@f0c1s/escape-html');
+const {escape: escapeJS} = require('@f0c1s/escape-javascript');
+const {rot47, rotations} = require('@f0c1s/security-rot');
+const base64 = require('@m1yh3m/base.64.codec.node.lib');
 
-const input = process.argv[2]
-console.log(green(`for input: ${blue(input)}`))
-let names = ['rot47', 'base64.encode', 'base64.decode', 'escapeJS.escape', 'escapeHTML.escape']
-let fns = [rot47, base64.encode, base64.decode, escapeJS.escape, escapeHTML.escape]
+const input = process.argv[2];
+console.log(green(`for input: ${blue(input)}`));
+let names = ['rot47', 'base64.encode', 'base64.decode', 'escapeJS', 'escapeHTML'];
+let fns = [rot47, base64.encode, base64.decode, escapeJS, escapeHTML];
 let out = fns.map(fn => {
-    let val = ''
+    let val = '';
     try {
-        val = fn(input)
+        val = fn(input);
     } catch (e) {
-        val = 'not applicable'
+        val = 'not applicable';
     }
-    return val
-})
+    return val;
+});
 out.forEach((v, i) => {
-    console.log(blue(input), green(names[i]), red(v))
-})
+    console.log(blue(input), green(names[i]), red(v));
+});
 
-console.log(`rot13 rotations on: ${blue(input)}`)
-rotations(input).forEach(i => console.log(i))
+console.log(`rot13 rotations on: ${blue(input)}`);
+rotations(input).forEach(i => console.log(i));
